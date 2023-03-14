3cd093dabd0602f02ca73ac163c330c5f0dff918
mfadlika <mfadlika@outlook.com>
2023-03-14 17:23:51 +0700

    main page updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,14 @@ import { ReactElement } from "react";
 
 export default function Home(): ReactElement {
   return (
-    <div className="flex justify-center items-center">
-      <span className="tagline linear-wipe">THROUGH THE WAVES</span>
+    <div>
+      <div className="flex justify-center items-center text-5xl">
+        <span className="tagline linear-wipe">THROUGH</span>
+      </div>
+      <div className="flex justify-center items-center text-5xl">
+        <span className="tagline linear-wipe">THE WAVES</span>
+      </div>
+      <Wave></Wave>
     </div>
   );
 }
