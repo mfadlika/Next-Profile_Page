d5ecd94ba9d44f52e5cc7146e1ea5b022eb543fb
mfadlika <mfadlika@outlook.com>
2023-03-14 17:43:09 +0700

    bug fixed in header styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,10 @@ import { ReactElement } from "react";
 export default function Home(): ReactElement {
   return (
     <div>
-      <div className="flex justify-center items-center text-5xl">
+      <div className="flex justify-center items-center text-8xl max-sm:text-7xl">
         <span className="tagline linear-wipe">THROUGH</span>
       </div>
-      <div className="flex justify-center items-center text-5xl">
+      <div className="flex justify-center items-center text-8xl max-sm:text-7xl">
         <span className="tagline linear-wipe">THE WAVES</span>
       </div>
       <Wave></Wave>
