476eeaad4dcb744c62b2f6070810d7451ab3d92c
mfadlika <mfadlika@outlook.com>
2023-03-19 21:07:46 +0700

    add country and city

--- src/pages/api/server.ts
+++ src/pages/api/server.ts
@@ -1,10 +1,15 @@
+import axios from "axios";
 import executeQuery from "../../../lib/db";
 
 export default async function postAPI(ip: string | string[], site: string) {
   try {
+    const { data } = await axios.get(`http://ip-api.com/json/${ip}`);
+
+    console.log(data);
     await executeQuery({
-      query: "INSERT INTO server_visitor (ip, site) VALUES(?, ?)",
-      values: [ip, site],
+      query:
+        "INSERT INTO server_visitor (ip, site, country, city) VALUES(?, ?)",
+      values: [ip, site, data["country"], data["city"]],
     });
   } catch (error) {
     console.log(error);
