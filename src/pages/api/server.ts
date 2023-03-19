f3752e759bcd9d4cf7882835ee009c685792203a
mfadlika <mfadlika@outlook.com>
2023-03-19 21:12:24 +0700

    add missing ?

--- src/pages/api/server.ts
+++ src/pages/api/server.ts
@@ -5,10 +5,9 @@ export default async function postAPI(ip: string | string[], site: string) {
   try {
     const { data } = await axios.get(`http://ip-api.com/json/${ip}`);
 
-    console.log(data);
     await executeQuery({
       query:
-        "INSERT INTO server_visitor (ip, site, country, city) VALUES(?, ?)",
+        "INSERT INTO server_visitor (ip, site, country, city) VALUES(?, ?, ?, ?)",
       values: [ip, site, data["country"], data["city"]],
     });
   } catch (error) {
